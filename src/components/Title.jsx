7ed57ed4c34d6329802e5146b8fcf9b94c1ea872
Dominic Hui <dominichui112@gmail.com>
2020-02-11 11:14:27 +0000

    added current logged in user to top of title bar

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../images/nc-news-logo.png";
+import { UserLogInContext } from "../contexts/UserLogInContext";
 
 // COMPONENT STYLING
 const TitleHeaderContainer = styled.header`
-  grid-template-columns: 150px 1fr;
+  grid-template-columns: 150px 1fr 100px;
   display: grid;
   height: 120px;
   background-color: #154854;
@@ -24,9 +25,40 @@ const Logo = styled.div`
 
 const Title = () => {
   return (
-    <TitleHeaderContainer>
-      <Logo></Logo>
-    </TitleHeaderContainer>
+    <UserLogInContext.Consumer>
+      {context => {
+        const { username, avatar_url, isLoggedIn } = context;
+
+        // USER ICON STYLING
+        const UserIcon = styled.div`
+          grid-column: 3/4;
+          margin-top: 20px;
+          height: 90px;
+          width: 90px;
+          background-image: url(${avatar_url});
+          background-size: 50px;
+          background-repeat: no-repeat;
+          background-position: top;
+          /* border: solid black; */
+          border-radius: 10px;
+          color: white;
+        `;
+
+        const UserIconName = styled.p`
+          margin-top: 60px;
+          text-align: center;
+        `;
+
+        return (
+          <TitleHeaderContainer>
+            <Logo></Logo>
+            <UserIcon>
+              <UserIconName>{username}</UserIconName>
+            </UserIcon>
+          </TitleHeaderContainer>
+        );
+      }}
+    </UserLogInContext.Consumer>
   );
 };
 
