f3f592b0c8be6a34299ab3aac69dc5dae5d077c6
Dominic Hui <dominichui112@gmail.com>
2020-02-13 08:41:27 +0000

    styled 404 error handler

--- src/components/ErrDisplayer.jsx
+++ src/components/ErrDisplayer.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 const ErrDisplayer = () => {
-  return <p>ERROR</p>;
+  return (
+    // <section>
+    //   Oops... Looks like you got lost
+    //   <button>Home</button>
+    // </section>
+
+    <section class="bg-purple">
+      <div class="stars">
+        <div class="central-body">
+          <img
+            class="image-404"
+            src="http://salehriaz.com/404Page/img/404.svg"
+          />
+        </div>
+        <div class="objects">
+          <img
+            class="object_rocket"
+            src="http://salehriaz.com/404Page/img/rocket.svg"
+            width="40px"
+          />
+          <div class="earth-moon">
+            <img
+              class="object_earth"
+              src="http://salehriaz.com/404Page/img/earth.svg"
+              width="100px"
+            />
+            <img
+              class="object_moon"
+              src="http://salehriaz.com/404Page/img/moon.svg"
+              width="80px"
+            />
+          </div>
+          <div class="box_astronaut">
+            <img
+              class="object_astronaut"
+              src="http://salehriaz.com/404Page/img/astronaut.svg"
+              width="140px"
+            />
+          </div>
+        </div>
+        <div class="glowing_stars">
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ErrDisplayer;
