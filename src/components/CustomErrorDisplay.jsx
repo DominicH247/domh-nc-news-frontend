1ed9bc2de62de297693bdc3041541a19bdd0e441
Dominic Hui <dominichui112@gmail.com>
2020-02-15 23:20:00 +0000

    added 500 error icon

--- src/components/CustomErrorDisplay.jsx
+++ src/components/CustomErrorDisplay.jsx
@@ -26,6 +26,7 @@ const CustomErrorDisplay = ({ msg, status }) => {
   const errorImg = {
     404: "https://image.flaticon.com/icons/svg/2298/2298239.svg",
     400: "https://image.flaticon.com/icons/svg/812/812867.svg",
+    500: "https://image.flaticon.com/icons/svg/2326/2326890.svg",
     Network: "https://image.flaticon.com/icons/svg/1201/1201519.svg"
   };
 
